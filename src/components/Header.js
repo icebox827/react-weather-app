21a9c403ba082b3ec53b94a534e2577e4b697be0
Denis Lafontant <icebox827@gmail.com>
2021-08-10 15:24:13 -0400

    Create Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Container, Grid, GridItem, Box } from "@chakra-ui/react"
 
 function Header() {
-
+  return (
+    <Container>
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+        <Box w="100%" h="10" bg="blue.500" />
+        <Box w="100%" h="10" bg="blue.500" />
+      </Grid>
+    </Container>
+  )
 }
+
+export default Header;
