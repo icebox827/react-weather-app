aa06b1397c5f2490975876f4f0cbc303cd98a4a6
Denis Lafontant <icebox827@gmail.com>
2021-08-10 17:22:19 -0400

    Update search box

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Flex, Button } from '@chakra-ui/react';
+import { Flex, Button, Center } from '@chakra-ui/react';
 
 function SearchArea() {
   return (
-    <Flex>
-      <main>
-        <form className="search-box">
-          <input 
-            type="text"
-            className="search-bar" 
-            name="city" 
-            id="city" 
-            placeholder="Please enter the city here!" 
-          />
-          <Button>Search</Button>
-        </form>
-      </main>
+    <Flex justify="center">
+      <form className="search-box">
+        <input 
+          type="text"
+          className="search-bar" 
+          name="city" 
+          id="city" 
+          placeholder="Please enter the city here!" 
+        />
+        <br />
+        <Button color="white" bg="blue.500">Search</Button>
+      </form>
     </Flex>
   )
 }
