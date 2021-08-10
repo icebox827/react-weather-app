17f9d7ccd40ed4cbfac84af106ae549739e2fc1f
Denis Lafontant <icebox827@gmail.com>
2021-08-10 17:06:28 -0400

    Add date to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Flex, Box, Heading, Center, Spacer, Menu, MenuItem } from '@chakra-ui/react';
+import { Flex, Box, Heading, Center, Spacer, Menu, MenuItem, Text } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
   return (
     <Flex >
-      <Menu>
       <Box w="100%" h="50" bg="blue.500" >
         <Center>
           <Heading color="white">
@@ -15,26 +14,10 @@ function Header() {
       </Box>
       <Spacer />
       <Box w="100%" h="50" bg="blue.500" p="2">
-        <MenuItem
-          className="social"
-          color="white"
-        >
-          <FontAwesomeIcon icon="github" />
-        </MenuItem>
-        <MenuItem
-          className="social"
-          color="white"
-        >
-          <FontAwesomeIcon icon="linkedin" />
-        </MenuItem>
-        <MenuItem
-          className="social"
-          color="white"
-        >
-         <FontAwesomeIcon icon="angellist" />
-        </MenuItem>
-        </Box>
-      </Menu>
+        <Text fontSize="lg" color="white" noOfLines={[1, 2, 3]} >
+          { ` Today is ${(new Date())} `}
+        </Text>
+      </Box>
     </Flex>
   )
 }
